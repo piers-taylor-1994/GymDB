8a73a8fea26781c6a03942b0948deb79ee655231
Piers Taylor <piers.taylorr@gmail.com>
2023-06-27 16:13:19 +0100

    Added order to Sets table

--- Workouts/Tables/Sets.sql
+++ Workouts/Tables/Sets.sql
@@ -6,6 +6,7 @@
     [Weight] VARCHAR(50) NULL, 
     [Sets] INT NULL, 
     [Reps] INT NULL,
+    [Order] INT NULL
 
     CONSTRAINT [FK_SetsRoutineId_ToRoutine_Id] FOREIGN KEY ([RoutineId]) REFERENCES [Workouts].[Routine](Id),
     CONSTRAINT [FK_SetsExerciseId_ToExercises_Id] FOREIGN KEY ([ExerciseId]) REFERENCES [Workouts].[Exercises](Id)
