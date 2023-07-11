ef46bd641ba87a76472ae17f9170ffd899cfcbe1
Piers Taylor <piers.taylorr@gmail.com>
2023-07-11 17:41:45 +0100

    Added RoutineTemplate tables

---
+++ Workouts/Tables/RoutineTemplate.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [Workouts].[RoutineTemplate]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [UserId] UNIQUEIDENTIFIER NOT NULL, 
+    [Name] VARCHAR(50) NOT NULL
+
+    CONSTRAINT [FK_RoutineTemplateUserId_ToUser_Id] FOREIGN KEY ([UserId]) REFERENCES [Users].[Users](Id)
+)
---
+++ Workouts/Tables/RoutineTemplateSets.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [Workouts].[RoutineTemplateSets]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [RoutineTemplateId] UNIQUEIDENTIFIER NOT NULL, 
+    [ExerciseId] UNIQUEIDENTIFIER NOT NULL
+
+    CONSTRAINT [FK_RoutineTemplateSetsRoutineTemplateId_ToRoutineTemplate_Id] FOREIGN KEY ([RoutineTemplateId]) REFERENCES [Workouts].[RoutineTemplate](Id)
+    CONSTRAINT [FK_RoutineTemplateSetsExerciseId_ToExercise_Id] FOREIGN KEY ([ExerciseId]) REFERENCES [Workouts].[Exercises](Id)
+)
