1773ceee69b54d51288755872ae87676b576ec85
Zoidika <jacobabrown1994@gmail.com>
2025-01-14 12:10:23 +0000

    Added initial swimming table

---
+++ Workouts/Tables/Swimming.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [Workouts].[Swimming]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [UserId] UNIQUEIDENTIFIER NOT NULL, 
+    [Date] DATETIME2 NOT NULL,
+    [Lengths] INT NOT NULL,
+    [TimeSwimming] INT NOT NULL,
+    [Review] BIT NOT NULL,
+    [Explanation] VARCHAR(MAX),
+
+    CONSTRAINT [FK_UserId_ToUser_Id] FOREIGN KEY ([UserId]) REFERENCES [Users].[Users](Id)
+)
